0590ebd5a67a54572180ab7c28ffa5020169d0be
keithdidier <keith.b.didier@gmail.com>
2017-09-29 13:53:35 -0600

    Made changes to server and added controlllers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-// const checkForSession = require('./middlewares/checkForSession');
+const massive = require('massive');
+const cors = require('cors');
+require('dotenv').config() ;
+const checkForSession = require('./middlewares/checkForSession');
+
+// Controllers
+const auth_controller = require('./controllers/auth_controller');
+const properties_controller = require('./controllers/properties_controller');
 
 const app = express();
+massive(process.env.CONNECTION_STRING).then(dbInstance => app.set('db', dbInstance));
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 app.use(session({
     secret: "qtyhgzefqacfemol",
     resave: false,
@@ -14,6 +23,19 @@ app.use(session({
 }));
 // app.use(checkForSession);
 app.use(express.static(`${__dirname}/../public/build`));
+
+// Endpoints
+    // Auth
+app.post('/api/auth/login', auth_controller.login);
+app.post('/api/auth/register', auth_controller.register);
+app.post('/api/auth/logout', auth_controller.logout);
+
+    // Properties
+app.post('/api/properties');
+app.get('/api/properties');
+app.delete('/api/properties/:id');
+// app.get('/api/properties/filter');
+
 
 // Express is helping my server to listen on port 3000
 const port = 3000;
